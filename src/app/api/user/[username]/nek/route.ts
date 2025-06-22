6acc7fc8480d9136a25cfbf43fbea1e31f164a13
tetsuhiro-dayo <119085878+tetuhiro-dayo@users.noreply.github.com>
2025-06-22 11:25:24 +0900

    何回やるんだこれ

--- src/app/api/user/[username]/nek/route.ts
+++ src/app/api/user/[username]/nek/route.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function POST(req: NextRequest, { params }: { params: { username: string } }) {
+export async function POST(req: NextRequest) {
     const { amount } = await req.json();
     if (typeof amount !== "number") return NextResponse.json({ error: "Amount must be a number" }, { status: 400 });
 
+    const username = new URL(req.url).pathname.split("/").at(-2);
+
     const user = await prisma.user.update({
-        where: { username: params.username },
+        where: { username: username },
         data: { nek: { increment: amount } },
     });
 
